ff4a133d1f733117eda0a46e2910add9b45ec041
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2023-12-30 01:15:32 +0200

    responsive changes

--- Test3 GUI/MyForm.h
+++ Test3 GUI/MyForm.h
@@ -7,7 +7,7 @@
 
 namespace Test3GUI {
 	const int arraySize = 500;
-	Trie<string> trie(arraySize);
+	Trie<string> trie;
 	using namespace System;
 	using namespace System::ComponentModel;
 	using namespace System::Collections;
@@ -60,6 +60,7 @@ namespace Test3GUI {
 	private: System::Windows::Forms::PictureBox^ pictureBox1;
 	private: System::Windows::Forms::PictureBox^ pictureBox2;
 	private: System::Windows::Forms::Label^ errorEmpty;
+	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel1;
 
 
 
@@ -87,16 +88,20 @@ namespace Test3GUI {
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
 			this->errorEmpty = (gcnew System::Windows::Forms::Label());
+			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			this->tableLayoutPanel1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(77, 56);
+			this->textBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Left | System::Windows::Forms::AnchorStyles::Right));
+			this->textBox1->Location = System::Drawing::Point(174, 101);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(249, 22);
+			this->textBox1->Size = System::Drawing::Size(165, 22);
 			this->textBox1->TabIndex = 0;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
 			// 
 			// label1
 			// 
@@ -109,11 +114,14 @@ namespace Test3GUI {
 			// 
 			// button1
 			// 
+			this->button1->Anchor = System::Windows::Forms::AnchorStyles::None;
+			this->button1->AutoSize = true;
+			this->button1->AutoSizeMode = System::Windows::Forms::AutoSizeMode::GrowAndShrink;
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button1->Location = System::Drawing::Point(139, 98);
+			this->button1->Location = System::Drawing::Point(208, 222);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(124, 40);
+			this->button1->Size = System::Drawing::Size(96, 39);
 			this->button1->TabIndex = 2;
 			this->button1->Text = L"Check";
 			this->button1->UseVisualStyleBackColor = true;
@@ -121,9 +129,10 @@ namespace Test3GUI {
 			// 
 			// pictureBox1
 			// 
+			this->pictureBox1->Anchor = System::Windows::Forms::AnchorStyles::Right;
 			this->pictureBox1->BackColor = System::Drawing::Color::Transparent;
 			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(33, 50);
+			this->pictureBox1->Location = System::Drawing::Point(130, 95);
 			this->pictureBox1->Name = L"pictureBox1";
 			this->pictureBox1->Size = System::Drawing::Size(38, 34);
 			this->pictureBox1->TabIndex = 3;
@@ -131,9 +140,10 @@ namespace Test3GUI {
 			// 
 			// pictureBox2
 			// 
+			this->pictureBox2->Anchor = System::Windows::Forms::AnchorStyles::Left;
 			this->pictureBox2->BackColor = System::Drawing::Color::Transparent;
 			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
-			this->pictureBox2->Location = System::Drawing::Point(33, 50);
+			this->pictureBox2->Location = System::Drawing::Point(345, 95);
 			this->pictureBox2->Name = L"pictureBox2";
 			this->pictureBox2->Size = System::Drawing::Size(38, 34);
 			this->pictureBox2->TabIndex = 4;
@@ -141,14 +151,44 @@ namespace Test3GUI {
 			// 
 			// errorEmpty
 			// 
+			this->errorEmpty->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
 			this->errorEmpty->AutoSize = true;
 			this->errorEmpty->BackColor = System::Drawing::Color::Transparent;
 			this->errorEmpty->ForeColor = System::Drawing::Color::Red;
-			this->errorEmpty->Location = System::Drawing::Point(77, 37);
+			this->errorEmpty->Location = System::Drawing::Point(182, 19);
 			this->errorEmpty->Name = L"errorEmpty";
-			this->errorEmpty->Size = System::Drawing::Size(186, 16);
+			this->errorEmpty->Size = System::Drawing::Size(148, 32);
 			this->errorEmpty->TabIndex = 5;
 			this->errorEmpty->Text = L"Please Enter a Word to Check";
+			this->errorEmpty->Click += gcnew System::EventHandler(this, &MyForm::errorEmpty_Click);
+			// 
+			// tableLayoutPanel1
+			// 
+			this->tableLayoutPanel1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->tableLayoutPanel1->BackColor = System::Drawing::Color::Transparent;
+			this->tableLayoutPanel1->ColumnCount = 3;
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				33.33333F)));
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				33.33333F)));
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				33.33333F)));
+			this->tableLayoutPanel1->Controls->Add(this->textBox1, 1, 1);
+			this->tableLayoutPanel1->Controls->Add(this->errorEmpty, 1, 0);
+			this->tableLayoutPanel1->Controls->Add(this->pictureBox1, 0, 1);
+			this->tableLayoutPanel1->Controls->Add(this->button1, 1, 2);
+			this->tableLayoutPanel1->Controls->Add(this->pictureBox2, 2, 1);
+			this->tableLayoutPanel1->Location = System::Drawing::Point(1, 2);
+			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
+			this->tableLayoutPanel1->RowCount = 4;
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 16.66667F)));
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 39.69298F)));
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 43.37363F)));
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 145)));
+			this->tableLayoutPanel1->Size = System::Drawing::Size(515, 456);
+			this->tableLayoutPanel1->TabIndex = 6;
 			// 
 			// MyForm
 			// 
@@ -157,17 +197,17 @@ namespace Test3GUI {
 			this->BackColor = System::Drawing::SystemColors::HotTrack;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
-			this->ClientSize = System::Drawing::Size(382, 302);
-			this->Controls->Add(this->errorEmpty);
-			this->Controls->Add(this->pictureBox2);
-			this->Controls->Add(this->pictureBox1);
-			this->Controls->Add(this->button1);
+			this->ClientSize = System::Drawing::Size(517, 461);
+			this->Controls->Add(this->tableLayoutPanel1);
 			this->Controls->Add(this->label1);
-			this->Controls->Add(this->textBox1);
+			this->MinimumSize = System::Drawing::Size(535, 508);
 			this->Name = L"MyForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterParent;
 			this->Text = L"MyForm";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
+			this->tableLayoutPanel1->ResumeLayout(false);
+			this->tableLayoutPanel1->PerformLayout();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -201,5 +241,9 @@ namespace Test3GUI {
 
 
 	
-	};
+	private: System::Void errorEmpty_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
